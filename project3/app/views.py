91b67813f17578b9d9b08adab86970069b691156
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-02-29 18:04:58 +0530

    Add files via upload

--- project3/app/views.py
+++ project3/app/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    d = {
+        'name':"Steve",
+        "age":28,
+        "pay":1500
+    }
+    return render(request, 'home.html', d)
 
 def login(request):
     return render(request, 'login.html')
 
 def register(request):
+    if request.method == 'POST':
+        return render(request, 'login.html')
     return render(request, 'register.html')
